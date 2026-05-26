5fe03a9a31240f43499b166dd8ad4ef5b758ed72
kartheesan03 <kartheesankumaresan@gmail.com>
2026-05-26 10:24:33 +0530

    Updated project

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -12,4 +12,15 @@ API.interceptors.request.use((req) => {
     return req;
 });
 
+API.interceptors.response.use(
+    (response) => response,
+    (error) => {
+        if (error.response && error.response.status === 401) {
+            localStorage.removeItem('userInfo');
+            window.location.href = '/login';
+        }
+        return Promise.reject(error);
+    }
+);
+
 export default API;
